8963679d4cca1d27b76a143105eba86bfb90aec6
Özgür YAŞA <ozguryasa@gmail.com>
2018-03-02 10:19:04 +0300

    Update for Heroku

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var middleware = require('./middleware.js');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(middleware.logger);
 app.get('/about', middleware.requireAuthentication, function(req, res) {
-    res.send('About Us');
+    res.send('About Us ');
 });
 app.use(express.static(__dirname + '/public'));
 console.log(__dirname);
